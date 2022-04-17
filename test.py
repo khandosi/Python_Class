4f84ae006bdbaf50aa9d5f07b14f163247ba372c
khandosi <khandosi@ensign.edu>
2022-04-16 20:26:17 -0600

    test update

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+# Ask questions
+name = input("Hello, what is your name: ")
+whereFrom = input("Where are you from: ")
+age = input("How old are you: ")
+
+# Print responses
+print("\nHello class,\nMy name is " + name + ",\nI grew up in " + whereFrom + ",\nI am " + age + " years old.")
