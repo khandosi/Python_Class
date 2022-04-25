269b887e0c6a81d2044698ab880dc127970c4fc5
khandosi <khandosi@ensign.edu>
2022-04-25 00:30:44 -0600

    update

--- Lab_3.py
+++ Lab_3.py
@@ -32,7 +32,33 @@ def cone_volume():
     volume = (1/3) * 3.14 * radius_sq * height
     print(volume)
 
-cone_volume()
+def weekly_hours():
+    hours = []
+    str1 = input("Enter the hours for Monday: ")
+    num1 = float(str1)
+    hours.append(num1)
+    str2 = input("Enter the hours for Tuesday: ")
+    num2 = float(str2)
+    hours.append(num2)
+    str3 = input("Enter the hours for Wednesday: ")
+    num3 = float(str3)
+    hours.append(num3)
+    str4 = input("Enter the hours for Thursday: ")
+    num4 = float(str4)
+    hours.append(num4)
+    str5 = input("Enter the hours for Friday: ")
+    num5 = float(str5)
+    hours.append(num5)
+    
+    print(sum(hours))
+
+def sq_pi():
+    power_str = input("Enter the power of pi: ")
+    power_float = float(power_str)
+    result = pow(3.1415926535897932384626433832795, power_float)
+    print(result)
+    
+
 
 #done = False
 #hours = []
