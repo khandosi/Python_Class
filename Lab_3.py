b7bea8cf07da07fdb7bc401a0b54a94a422f265f
khandosi <khandosi@ensign.edu>
2022-04-25 00:03:37 -0600

    update

--- Lab_3.py
+++ Lab_3.py
@@ -1,45 +1,58 @@
 
 
-print('Which of the following would you like to calculate?')
+import math
+
+print('\nWhich of the following would you like to calculate?')
 option = ['Area of a circle','MPG for a vehicle','Total number of hours worked in a week','Hypotenuse of a triangle','Placeholder']
 for x in option:
         print(x)
+choice = input('\nChoose 1, 2, 3, 4, or 5: ')
 
 
-#import random
-#
-#def goAgain():
-#    str = input("Would you like to play again? (y/n):")
-#    if str == 'y':
-#        return 'yes'
-#    elif str == 'n':
-#        return 'no'
+def c_area():
+    str1 = input("Enter the diameter: ")
+    int1 = int(str1)
+    area = int1 * 3.14
+    print(area)
+
+def mpg():
+    str1 = input("Enter the distance traveled: ")
+    dist = int(str1)
+    str2 = input("Enter the number of gallons used: ")
+    gallons = int(str2)
+    mpg = dist / gallons
+    print(mpg)
+
+def cone_volume():
+    str1 = input("Enter the radius: ")
+    radius = int(str1)
+    str2 = input("Enter the height: ")
+    height = int(str2)
+    radius_sq = pow(radius, 2)
+    volume = (1/3) * 3.14 * radius_sq * height
+    print(volume)
+
+cone_volume()
+
+#done = False
+#hours = []
+
+#while done == False:
+#    num_1 = input("Enter a number: ")
+#    if num_1 == '':
+#        done = True
 #    else:
-#        return None
-#
-#goAgain()
-import math
+#        hours.append(num_1)
 
-#def c_area(int1):
-#    sum1 = int1 * 3.14
-#    return sum1
 
-#def mpg(int1,int2):
-#    sum1 = int1 / int2
-#    return sum1
+if choice == 1:
+    c_area()
+elif choice == 2:
+    mpg()
 
-done = False
-hours = []
+c_area()
 
-while done == False:
-    num_1 = input("Enter a number: ")
-    if num_1 == '':
-        done = True
-    else:
-        hours.append(num_1)
-
-print(sum(hours))
-
+mpg()
 
 
 
