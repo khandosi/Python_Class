e0dabbfb37505fe9653913d804dea7413fde60d1
khandosi <103863028+khandosi@users.noreply.github.com>
2022-05-06 21:37:55 -0600

    Update Lab_4.py

--- Lab_4.py
+++ Lab_4.py
@@ -1,16 +1,27 @@
-num_list = []
-finished = 'n'
+# Make a function to remove duplicates
+def rem_dups(list):
+    dedup_list = []
+    for number in list:
+        if number not in dedup_list:
+            dedup_list.append(number)
+    return dedup_list
 
+# Initialize the list
+int_list = []
 
+# Ask for whole numbers in a loop until ''
 while True:
-    print('Enter a number: (blank to quit)')
-    num = input()
-    if num == '':
+    print('Enter a whole number, or enter nothing to quit')
+    int_input = input()
+    if int_input == '':
         break
-    else:
-        
-    
+    int_list = int_list + [int_input]
 
+# Remove duplicates and sort
+int_list = rem_dups(int_list)
+int_list.sort()
 
-#print(sum(num_list))
-
+# Loop through list contents and print
+print('The numbers entered were: ')
+for int_input in int_list:
+    print(int_input)
