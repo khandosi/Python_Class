27befe928a4852fc9d3eb69950c113fce7e078d6
khandosi <khandosi@ensign.edu>
2022-05-07 23:33:38 -0600

    update

--- Lab_5.py
+++ Lab_5.py
@@ -1,11 +1,44 @@
-valid_color = {'Black', 'White'}
-valid_pieces = {'King', 'Queen', 'Bishop', 'Knight', 'Rook', 'Pawn'}
-valid_location = {'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'}
+valid_color = ['Black', 'White']
+short_colors = {'Black': 'b', 'White': 'w'}
+valid_pieces = ['King', 'Queen', 'Bishop', 'Knight', 'Rook', 'Pawn']
+short_names = {'King': 'k', 'Queen': 'q', 'Bishop': 'b', 'Knight': 'n', 'Rook': 'r', 'Pawn': 'p'}
+max_pieces = {'King': 1, 'Queen': 1, 'Bishop': 2, 'Knight': 2, 'Rook': 2, 'Pawn': 8}
+valid_x = [1,2,3,4,5,6,7,8]
+valid_y = ['a','b','c','d','e','f','g','h']
+
+
 
 while True:
-    check = input('Please enter a Chess color, piece, or location (or nothing to quit): ')
-    if check == '':
+    print('Please enter a Chess color. Choose from: ')
+    for i in valid_color:
+        print(i)
+    check_color = input()
+    if check_color == '':
         break
-
-    if (check in valid_color or check in valid_pieces or check in valid_location):
+    if (check_color in valid_color or check_color in short_colors):
         print('found')
+
+    print('Please enter a Chess piece. Choose from: ')
+    for i in valid_pieces:
+        print(i)
+    check_name = input()
+    if check_name == '':
+        break
+    if (check_name in valid_pieces or check_name in short_names):
+        print('found')
+
+    loc = input('Please enter a coordinate in algebraic chess notation: ')
+    if len(loc) == 2: 
+        if loc[:1] in valid_y:
+            print('Valid')
+        loc_int = int(loc[1:])
+        if loc_int in valid_x:
+            print('Valid')
+
+
+    #check_loc = input('Please enter a Chess cooridate:')
+    #if check_loc == '':
+    #    break
+#
+    #if (check_loc in valid_color or check in short_colors or check in valid_pieces or check in short_names or check):
+    #    print('found')
