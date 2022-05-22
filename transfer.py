9793ff3aa41efefb723403769be6046b3f085281
khandosi <103863028+khandosi@users.noreply.github.com>
2022-05-21 20:28:37 -0600

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,35 +1,20 @@
-import random as r
-import string
+# Open the file and read the contents
+f = open(input('Enter the original file name: '))
+test = f.readlines()
+lines = []
+# Add lines to an array and strip off new line characters
+for line in test:
+    new_line = line[:-2]
+    lines.append(new_line)
+f.close
 
-#def make_file(file):
-#    f = open(file,'w')
+# Get the target file name
+f2 = open(input('Enter the new file name: '))
+i = 1
+for line in lines:
+    f.write('1' + '. ' + '{}' + ' sample'.format(line))
+    i += 1
+f2.close()
 
-def addLetters():
-    random = r.choice(string.ascii_letters)
-    return random 
-
-def addIntegers():
-    random = r.sample(range(0, 9999))
-    return random
-
-def addFloat():
-    random = round(r.uniform(0, 100000), 2)
-    return random
-
-def main():
-    input_file = input('Enter a file: ')
-    #make_file(input_file)
-    f = open(input_file, "a")
-    
- 
-
-    letter = addLetters()
-    integer = addIntegers()
-    floater = addFloat()
-
-    #i = 1
-    #while i < 20:
-    #    f.write('{}\n').format(letter)
-    #    i += 1
-
-main()
+#with open("test.txt", "r") as f:
+#    print(f.readlines([:-2]))
