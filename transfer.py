460d492b071aa0b7b2f3d69092ab5f17b78fd97c
khandosi <103863028+khandosi@users.noreply.github.com>
2022-06-21 22:13:13 -0700

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,20 +1,52 @@
-# Open the file and read the contents
-f = open(input('Enter the original file name: '))
-test = f.readlines()
-lines = []
-# Add lines to an array and strip off new line characters
-for line in test:
-    new_line = line[:-2]
-    lines.append(new_line)
-f.close
+import socket
+from time import sleep
 
-# Get the target file name
-f2 = open(input('Enter the new file name: '))
-i = 1
-for line in lines:
-    f.write('1' + '. ' + '{}' + ' sample'.format(line))
-    i += 1
-f2.close()
+# Initialize variables
+target_host = input("Please enter the target IP: ")
+save_file = input("Enter the name of the file you want to write the scans to: ")
 
-#with open("test.txt", "r") as f:
-#    print(f.readlines([:-2]))
+results = []
+
+# Define port range, set timeout to 0.1 (This is POC, in real world change to suit your latency), open a TCP socket for the specified port, attempt a connection over the specified port. For TCP, a result of 0 indicates a connection was successful. Lastly, close the connection.
+def tcp_scan():
+
+    for port in range(7995, 8005):
+        socket.setdefaulttimeout(0.1)
+        print("Scanning port " + str(port) + " on " + target_host)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = client.connect_ex((target_host, port))
+        if result == 0:
+            results.append("\nOpen connection found on TCP port " + str(port))
+        client.close()
+    return results
+
+
+# Define port range, set timeout to 0.1 (This is POC, in real world change to suit your latency), open a UDP socket for the specified port, attempt a connection over the specified port. Lastly, close the connection.
+def udp_scan():
+
+    for port in range(7995, 8005):
+        print("Scanning port " + str(port) + " on " + target_host)
+        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket.setdefaulttimeout(0.1)
+        result = client.connect_ex((target_host, port))
+        if result == 0:
+            results.append("\nOpen connection found on UDP port " + str(port))
+    client.close()
+    return results
+
+
+# Initiate the scans
+print("\nStarting TCP scan\n")
+sleep(1)
+tcp_scan()
+print("\nStarting UDP scan\n")
+sleep(1)
+udp_scan()
+
+# Append results to the specified file, then close the file
+f = open(save_file, "a")
+
+for line in results:
+    f.write(line)
+
+f.close()
